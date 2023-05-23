4310020257c50626219257d4d221a97b28586e2e
Fredenh <112377991+Fredenh@users.noreply.github.com>
2023-05-23 22:08:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+# Create virtual environment
+python3 -m venv assignment3_env
+
 # then install requirements
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-
-# run the code 
-python3 src/a3short.py
